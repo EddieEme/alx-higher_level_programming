596a03fe7addaab5ed5570c0352f4094c113b853
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-11-30 19:49:05 +0000

    0-add.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+# for i in range(ord('z'), ord('A') - 1, -1):
+#   if 91 <= i <= 96:
+#     continue
+#   print(chr(i) if i % 2 == 0 else chr(i - 32), end="")
+
 for i in range(ord('z'), ord('A') - 1, -1):
-  print(chr(i) if i % 2 == 0 else chr(i - 32), end="")
+    converted_char = chr(i) if i % 2 == 0 else chr(i - 32)
+    if 91 <= ord(converted_char) <= 96:
+        continue
+    print(converted_char, end="")
