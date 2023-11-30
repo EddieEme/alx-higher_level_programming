8edbb975f2b7e31cf04c6a90065cdfadefbc613d
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-11-30 21:13:31 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+
+arg = sys.argv
+
+count = f"{len(arg) - 1}"
+
+if count == 0:
+    print("{} arguments".format(count))
+else:
+    print("{} arguments".format(count))
+    for i in range(1, len(arg)):
+        print("{}: {}".format(i, arg[i]))
+
