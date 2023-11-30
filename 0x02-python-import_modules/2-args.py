52c14db1959cbc58929427718b1ddc3a003536de
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-11-30 21:19:06 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ arg = sys.argv
 count = len(arg) - 1
 
 if count == 0:
-    print("{} arguments".format(count))
+    print("{} arguments.".format(count))
 else:
-    print("{} arguments".format(count))
+    print("{} arguments:".format(count))
     for i in range(1, len(arg)):
         print("{}: {}".format(i, arg[i]))
