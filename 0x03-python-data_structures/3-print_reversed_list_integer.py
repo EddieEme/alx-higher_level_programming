1d16c6500790aab919e6c98e0752bed4b593c239
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-12-03 21:46:51 +0000

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in reversed(my_list):
-        print("{}".format(i))
+    for item in reversed(my_list):
+        return item
+
