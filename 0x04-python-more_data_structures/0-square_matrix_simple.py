704c8cad701c3f6e7268ffcc1ed6354670174324
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-12-06 21:41:47 +0000

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_row = []
+        for j in row:
+            new_row.append(j * j)
+        new_matrix.append(new_row)
+    return new_matrix
