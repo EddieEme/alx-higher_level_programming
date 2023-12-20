b7518d4daacc881bf10b1677c07a73f076890c72
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-12-20 22:11:27 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""An class Square that defines a square
+private instance attribute size
+"""
+
+
+class Square:
+    """An class Square that defines a square
+    private instance attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
