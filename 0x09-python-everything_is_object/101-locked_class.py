37bf26253e95b4dd36c1a2ea4a1cacaff93fc9b6
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2024-01-06 00:19:44 +0000

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,7 +3,7 @@
 
 
 class LockedClass:
-	"""
-	Only allows instace attribute called first_name
-	"""
-	__slots__ = ["first_name"]
+    """
+    Only allows instace attribute called first_name
+    """
+    __slots__ = ["first_name"]
