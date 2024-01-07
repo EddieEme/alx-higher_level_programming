68db0f590485cac747b3d9609cd8b00b8e063df8
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2024-01-07 20:02:34 +0000

    advanced task

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Define a function that multiplies two matrices"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): first matrix.
+        m_b (list of lists of ints/floats): second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
