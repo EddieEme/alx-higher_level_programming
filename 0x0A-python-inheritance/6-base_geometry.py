ef20749a77ded164e001f8925be71724d4a17b68
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2024-01-10 23:53:25 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """my class BaseGeometry"""
+
+    def area(self):
+        """area() is not implemented"""
+        raise Exception("area() is not implemented")
