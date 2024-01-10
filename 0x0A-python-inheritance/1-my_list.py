8eca412a58c14f44bc9e6f2ecd625190fc0e2f6a
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2024-01-10 21:22:24 +0000

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define class MyList that inherits from list"""
+
+
+class MyList(list):
+    """class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """that prints the list, but sorted (ascending sort)"""
+
+        print(sorted(self))
