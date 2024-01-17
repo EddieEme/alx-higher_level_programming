b0ba13a5094c77b0e6e302bb1db98e2ec82c053a
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2024-01-17 19:02:44 +0100

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define base of all classes"""
+import json
 
 
 class Base:
@@ -13,3 +14,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries: is a list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            to_json_string = json.dumps(list_dictionaries)
+            return to_json_string
