05aee121ba1c51f2cadc36a48510a78358e90326
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-03-11 14:05:17 +0100

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that prints a message depending of the number of arguments passed
+const { argv } = require('process');
+
+if (argv.length < 3) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
