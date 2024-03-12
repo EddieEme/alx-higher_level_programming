26308f64ab46fc1bef54ee734f433a1d008c15e7
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-03-12 22:36:47 +0100

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 // Write a script that prints x times “C is fun”
 
 const x = process.argv[2];
-
 if (!parseInt(x)) {
-	consle.log('Missing number of occurrences');
-	} else {
-		for (let i = 0; i < x; i++) {
-			console.log('C is fun');
-		}
-	}
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
