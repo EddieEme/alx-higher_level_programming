550f5015bd5914af65468c78fafae314ba49132b
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-04-23 23:32:17 +0100

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+//script that reads and prints the content of a file.
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+
+// Read the file with error handling
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err); // Print the error object
+  } else {
+    console.log(data);
+  }
+});
