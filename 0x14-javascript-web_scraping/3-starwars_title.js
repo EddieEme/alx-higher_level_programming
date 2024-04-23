56334b46724ac95411659d9fb05a21dc0475ec59
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-04-24 00:17:32 +0100

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Write a script that prints the title of a Star Wars 
+//movie where the episode number matches a given integer.
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+// Build the API URL
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make the GET request
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+    } else {
+        const data = JSON.parse(body);
+        if (data.title) {
+            console.log(data.title);
+        } else {
+            console.error(`${movieId}`);
+        }
+    }
+});
