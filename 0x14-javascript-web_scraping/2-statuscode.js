4db7b3c427d5830444f4d0d9fbbe316919042e5b
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-04-24 00:11:32 +0100

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(`code: ${response.statusCode}`);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
