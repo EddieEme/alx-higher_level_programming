6396851207f11698fca47ca86694d506a7abdb6c
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-04-24 01:27:49 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,33 +3,22 @@
 // of movies where the character “Wedge Antilles” is present.
 
 const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-const fileApi = 'https://swapi-api.alx-tools.com/api/films/';
-
-// Wedge Antilles ID
-const characterId = 18;
-
-function countMoviesWithCharacter(url, callback) {
-  request(url, (error, response, body) => {
-    if (error) {
-      callback(error);
-    } else {
-      try {
-        const data = JSON.parse(body);
-        const moviesWithCharacter = data.results.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}`));
-        callback(null, moviesWithCharacter.length); // Pass null for error, count for success
-      } catch (error) {
-        callback(error);
-      }
-    }
-  });
-}
-
-
-countMovieWithCharacter(fileApi, (error, movieCount) => {
+request.get(url, (error, response, body) => {
     if (error) {
         console.log(error);
     } else {
-        console.log('${movieCount}');
+        const data = JSON.parse(body);
+        data.results.forEach((film) => {
+            film.characters.forEach((character) => {
+                if (character.includes(characterId)) {
+                    count += 1;
+                }
+            });
+        });
+        console.log(count);
     }
 });
